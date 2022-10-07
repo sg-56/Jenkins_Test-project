a20c3854a82dd017cb2665dbb8422f877732b85f
sg-56 <55098139+sg-56@users.noreply.github.com>
2022-10-07 16:17:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.50)
 
 #Using decision tree algorithm
-from sklearn import tree
+from sklearn.tree import BaseDecisionTree as tree
 classifier=tree.DecisionTreeClassifier()
 classifier.fit(x_train,y_train)
 predictions=classifier.predict(x_test)
